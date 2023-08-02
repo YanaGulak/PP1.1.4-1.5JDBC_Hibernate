b6a3ac15772cb5e699536a5a66eaaa97f4f7a803
Yana <yana_mashuta@mail.ru>
2023-08-02 15:01:31 +0500

    Check and change

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -5,13 +5,12 @@ import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     private UserDao userDao = new UserDaoJDBCImpl();
 
-    public void createUsersTable() throws SQLException {
+    public void createUsersTable() {
         userDao.createUsersTable();
     }
 
@@ -19,18 +18,18 @@ public class UserServiceImpl implements UserService {
         userDao.dropUsersTable();
     }
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
+    public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
     }
 
-    public void removeUserById(long id) throws SQLException {
+    public void removeUserById(long id) {
         userDao.removeUserById(id);
 
     }
 
-    public List<User> getAllUsers() throws SQLException {
+    public List<User> getAllUsers() {
         List<User> allUsers = userDao.getAllUsers();
-         allUsers.forEach(System.out::println);
+        allUsers.forEach(System.out::println);
         return allUsers;
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,7 +1,6 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -9,14 +8,15 @@ public class Util {
     private static final String DB_Driver = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost/usersql";
     private static final String DB_User = "root";
-    private static final String DB_Pass = "ilovejava";
+    private static final String DB_PASS = "ilovejava";
+
 
     public static Connection getConnection() {
         Connection connection = null;
 
         try {
             Class.forName(DB_Driver);
-            connection = DriverManager.getConnection(DB_URL, DB_User, DB_Pass);
+            connection = DriverManager.getConnection(DB_URL, DB_User,DB_PASS);
             System.out.println("Get connection successful");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Connection ERROR");
